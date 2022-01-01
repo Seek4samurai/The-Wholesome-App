861c202c70930f24e126bec120005346f0c6a9cf
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-01-01 15:20:09 +0530

    Submission now returns array, 🛑Checkpoint!

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,3 +1,13 @@
 exports.home = (req, res) => {
   res.render("main")
 };
+
+exports.uploads = (req, res, next) => {
+  const files = req.files;
+  if (!files) {
+    const error = new Error('Please choose a file');
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  res.json(files);
+};
---
+++ server/middleware/multer.js
@@ -0,0 +1,14 @@
+const multer = require('multer');
+
+// Set Storage
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'server/uploads')
+  },
+  filename: function (req, file, cb) {
+    var ext = file.originalname.substring(file.originalname.lastIndexOf('.'));
+    cb(null, file.fieldname + '-' + Date.now() + ext);
+  }
+});
+
+module.exports = store = multer({ storage: storage });
