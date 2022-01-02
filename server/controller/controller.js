08767da733f9aadc69850efba10ce054ff371ffa
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-01-02 17:59:06 +0530

    added Schema

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,3 +1,6 @@
+const UploadModel = require('../model/schema');
+const fs = require('fs')
+
 exports.home = (req, res) => {
   res.render("main")
 };
@@ -6,8 +9,22 @@ exports.uploads = (req, res, next) => {
   const files = req.files;
   if (!files) {
     const error = new Error('Please choose a file');
+
     error.httpStatusCode = 400;
     return next(error);
   }
+  // Images to base64
+  let imgArray = files.map((file) => {
+    let img = fs.readFileSync(file.path);
+    console.log(img);
+    return encodeImg = img.toString('base64')
+  })
+
+  imgArray.map((src, index) => {
+    // object to store
+    let finalImg = {
+      filename: files
+    }
+  });
   res.json(files);
 };
